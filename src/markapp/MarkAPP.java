8669121d2587788b7ff0066c1f113b28cbea4580
SGalvez88 <sergio.galvez.reguera@iestubalcain.net>
2023-02-03 20:08:35 +0100

    first commit

--- src/markapp/MarkAPP.java
+++ src/markapp/MarkAPP.java
@@ -8,6 +8,7 @@ import com.marklogic.client.io.SearchHandle;
 import com.marklogic.client.io.StringHandle;
 import com.marklogic.client.query.MatchDocumentSummary;
 import com.marklogic.client.query.QueryManager;
+import com.marklogic.client.query.QueryManager.QueryView;
 import com.marklogic.client.query.StructuredQueryBuilder;
 import com.marklogic.client.query.StructuredQueryDefinition;
 
@@ -19,19 +20,30 @@ public class MarkAPP {
     private static final String PASSWORD = "1234";
 
     public static void main(String[] args) {
-        // Crea una conexión a la base de datos "prueba" de MarkLogic
-        DatabaseClient client = DatabaseClientFactory.newClient(HOST, PORT, USERNAME, PASSWORD, DatabaseClientFactory.Authentication.DIGEST);
 
-        // Crea un gestor de documentos XML
+        DatabaseClient client = DatabaseClientFactory.newClient(HOST, PORT, USERNAME, PASSWORD, DatabaseClientFactory.Authentication.DIGEST);
+         // Crea un gestor de documentos XML
         XMLDocumentManager xmlManager = client.newXMLDocumentManager();
+                
+//        crearUsuarios(client,xmlManager);
+        
+//        queryComprobarSiExiste(client);
+        
+        
+        getAll(client);
+       
+    }
+    
+    
+    public static void crearUsuarios(DatabaseClient client, XMLDocumentManager xmlManager){
 
         // Define los datos de usuario en formato XML
         String xmlContent = "<user>"
-                + "<name>Sergio</name>"
-                + "<email>sergio@gmail.com</email>"
+                + "<name>Jairo</name>"
+                + "<email>Jairo@gmail.com</email>"
                 + "</user>";
 
-        String docId = "/users/user1.xml";
+        String docId = "/users/user4.xml";
 
         // Define the metadata for the document
         DocumentMetadataHandle metadata = new DocumentMetadataHandle();
@@ -42,10 +54,51 @@ public class MarkAPP {
 
         // Guarda los datos XML en la base de datos "prueba"
         xmlManager.write(docId, metadata, handle);
-     
+    }
+    
+    public static void getAll(DatabaseClient client){
+        
+         // Create a query manager
+      QueryManager queryMgr = client.newQueryManager();
 
-        // Cierra la conexión a la base de datos
+      // Define a query to retrieve all documents in the "users" collection
+      StructuredQueryBuilder qb = queryMgr.newStructuredQueryBuilder();
+      StructuredQueryDefinition query = qb.collection("users");
+
+      // Search for all matching documents
+      StringHandle results = new StringHandle();
+      queryMgr.search(query, results);
+
+      // Display the results
+      System.out.println(results.get());
+
+      // Release the client
+      client.release();
+    
+    }
+    
+    
+    public static void queryComprobarSiExiste(DatabaseClient client) {
+
+        //Este metodo devuelve si hay un usuario segun la query, en este caso devuelve que existe 1
+        // Crear un gestor de consultas
+        QueryManager queryMgr = client.newQueryManager();
+
+        // Crear una definición de consulta estructurada
+        StructuredQueryBuilder qb = queryMgr.newStructuredQueryBuilder();
+        StructuredQueryDefinition query = qb.and(qb.term("username", "Jairo"));
+
+        // Ejecutar la consulta y obtener el resultado
+        SearchHandle results = queryMgr.search(query, new SearchHandle());
+
+        // Mostrar los resultados
+        System.out.println("Results: " + results.getTotalResults());
+
+        // Cerrar la conexión
         client.release();
 
     }
+    
+    
+    
 }
